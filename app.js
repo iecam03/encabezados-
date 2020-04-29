58760d2935d4a06ad6c7910ea17366d95ea78408
Camilo <iecam03@gmail.com>
2020-04-28 20:22:13 -0500

    1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    var header = req.headers['user-agent']
+    res.send(header)
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
